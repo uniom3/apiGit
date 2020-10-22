f3f7f3c30b31719eea24787308b9490013a440f0
uniom3 <deivid.mendonca@hotmail.com>
2020-10-22 17:54:06 -0300

    corrigindo

--- src/main/java/com/example/mendonca/web/MainController.java
+++ src/main/java/com/example/mendonca/web/MainController.java
@@ -3,8 +3,10 @@ package com.example.mendonca.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller 
+@RequestMapping("/")
 public  class  MainController {
 
 	   @GetMapping("/")
