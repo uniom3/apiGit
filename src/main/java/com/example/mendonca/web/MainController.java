c3d7ae4eb2dd59baa4cd92816eb5617462c0b0d6
uniom3 <deivid.mendonca@hotmail.com>
2020-10-23 15:13:55 -0300

    corrigindo

--- src/main/java/com/example/mendonca/web/MainController.java
+++ src/main/java/com/example/mendonca/web/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller 
 public  class  MainController {
 
-	   @GetMapping("/")
+	   @GetMapping()
 	    public String root(){
 	        return "index";
 	    }
